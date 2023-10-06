3e3255c2d39025a15df732ede8c989f114aedad0
Nazly Zambrano <nazmi.016@gmail.com>
2023-10-06 16:19:10 -0500

    learning filet y .find

--- src/bases/08-filter-find.js
+++ src/bases/08-filter-find.js
@@ -1,11 +1,23 @@
 import { princess } from "./data/princess";
 
+// const getPrincessById = (id) => {
+//   return princess.find((princess) => {
+//     if (princess.id === id) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   });
+// };
+
+// console.log(getPrincessById(2));
+
 const getPrincessById = (id) => princess.find((princess) => princess.id === id);
 console.log(getPrincessById(5));
 
 const getPrincessByName = (name) =>
   princess.find((princess) => princess.name === name);
-console.log(getPrincessByName("Olaf"));
+// console.log(getPrincessByName("Olaf"));
 
 const getPrincessByMovie = (movie) =>
   princess.filter((princess) => princess.movie === movie);
---
+++ src/data/princess.js
@@ -0,0 +1,27 @@
+export const princess = [
+  {
+    id: 1,
+    name: "Vanellope",
+    movie: "Ralph El Demoledor",
+  },
+  {
+    id: 2,
+    name: "Ohana",
+    movie: "Ohana",
+  },
+  {
+    id: 3,
+    name: "Rapunzel",
+    movie: "Enredados",
+  },
+  {
+    id: 4,
+    name: "Elsa",
+    movie: "Frozen",
+  },
+  {
+    id: 5,
+    name: "Olaf",
+    movie: "Frozen",
+  },
+];
